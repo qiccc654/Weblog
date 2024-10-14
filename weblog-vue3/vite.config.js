6fe2b81c43028deb8b786c6739a4dccd07d1b5a0
qiccc <qiccc654@foxmail.com>
2024-10-14 19:51:16 +0800

    第七次提交 基本完成登录界面,实现前端空表单校验

--- weblog-vue3/vite.config.js
+++ weblog-vue3/vite.config.js
@@ -8,6 +8,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+	    proxy: {
+	      '/api': {
+	        target: 'http://localhost:8080',
+	        changeOrigin: true,
+	        rewrite: (path) => path.replace(/^\/api/, ''),
+	      },
+	    }
+	  },
   plugins: [
     vue(),
     AutoImport({
